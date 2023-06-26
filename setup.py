536589e73a84bef3b3600b5625322a6199b00887
ROSBot Marcel <dev@phntm.io>
2023-06-26 05:54:18 +0000

    necessary to run pip install -e

--- setup.py
+++ setup.py
@@ -11,4 +11,34 @@ cffi_modules = [
 if os.environ.get("READTHEDOCS") == "True":
     cffi_modules = []
 
-setuptools.setup(cffi_modules=cffi_modules)
+install_requires = [
+    "aioice>=0.9.0,<1.0.0",
+    "av>=9.0.0,<11.0.0",
+    "cffi>=1.0.0",
+    "cryptography>=2.2",
+    'dataclasses; python_version < "3.7"',
+    "google-crc32c>=1.1",
+    "pyee>=9.0.0",
+    "pylibsrtp>=0.5.6",
+    "pyopenssl>=23.1.0",
+]
+
+extras_require = {
+    'dev': [
+        'aiohttp>=3.7.0',
+        'coverage>=5.0',
+        'numpy>=1.19.0',
+    ]
+}
+
+setuptools.setup(
+    cffi_modules=cffi_modules,
+    name='aiortc',
+    version='1.5.0',
+    package_dir={"": "src"},
+    packages=["aiortc", "aiortc.codecs", "aiortc.contrib"],
+    python_requires=">=3.7",
+    setup_requires=["cffi>=1.0.0"],
+    install_requires=install_requires,
+    extras_require=extras_require,
+    )
